b562b10a197fa0045d93f7a3b8c23c096ef25525
Koichi Nakashima <koichi@nksm.name>
2020-08-17 01:03:23 +0900

    Change default error name

--- README.md
+++ README.md
@@ -213,7 +213,7 @@ Example
 # $3-: Validation arguments
 error() {
   case $2 in
-    unknown) echo "unrecognized option '$1'" ;;
+    @unknown) echo "unrecognized option '$1'" ;;
     number) echo "option '$1' is not a number" ;;
     range) echo "option '$1' is not a number or out of range ($3 - $4)" ;;
     *) return 1 ;; # Display default error
--- getoptions_only.sh
+++ getoptions_only.sh
@@ -79,7 +79,7 @@ getoptions() {
     quote on "$on"
     quote off "$off"
     _3 "$sw)"
-    _4 '[ "${OPTARG:-}" ] && set -- "$1" noarg && break'
+    _4 '[ "${OPTARG:-}" ] && set -- "$1" @noarg && break'
     _4 "eval '[ \${OPTARG+x} ] &&:' && OPTARG=$on || OPTARG=$off"
     [ "$validate" ] && _4 "$validate || { set -- \"\$1\" $validate; break; }"
     [ "$counter" ] && code="\$((\${$1:-0} + \$OPTARG))" || code='$OPTARG'
@@ -89,7 +89,7 @@ getoptions() {
   param() {
     args "$@"
     _3 "$sw)"
-    _4 '[ $# -le 1 ] && set -- "$1" required && break'
+    _4 '[ $# -le 1 ] && set -- "$1" @required && break'
     _4 'OPTARG=$2'
     [ "$validate" ] && _4 "$validate || { set -- \"\$1\" $validate; break; }"
     code "$1" _4 "$1=\$OPTARG" "${1#:}"
@@ -147,7 +147,7 @@ getoptions() {
   _2 'esac'
   _2 'case $1 in'
   "$@"
-  _3 "[-${_plus:++}][!-]*)" 'set -- "$1" unknown && break ;;'
+  _3 "[-${_plus:++}][!-]*)" 'set -- "$1" @unknown && break ;;'
   restargs() {
     _3 "$1"
     _4 'while [ $# -gt 0 ]; do'
@@ -168,9 +168,9 @@ getoptions() {
   _1 '[ $# -eq 0 ] && return 0'
   [ "$_error" ] && _1 "$_error" '"$@" >&2 && exit 1'
   _1 'case $2 in'
-  _2 "unknown) echo \"unrecognized option '\$1'\" ;;"
-  _2 "noarg) echo \"option '\$1' doesn't allow an argument\" ;;"
-  _2 "required) echo \"option '\$1' requires an argument\" ;;"
+  _2 "@unknown) echo \"unrecognized option '\$1'\" ;;"
+  _2 "@noarg) echo \"option '\$1' doesn't allow an argument\" ;;"
+  _2 "@required) echo \"option '\$1' requires an argument\" ;;"
   _2 "*) echo \"option '\$1' validation error: \$2\""
   _1 'esac >&2'
   _1 'exit 1'
