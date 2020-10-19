947cdfcb7606408495c92595c8cb4658a385b016
Cem Keylan <cem@ckyln.com>
2020-10-20 00:54:01 +0300

    getoptions: unset OPTARG only when returning normally

--- getoptions.sh
+++ getoptions.sh
@@ -170,8 +170,7 @@ getoptions() {
   _2 'esac'
   _2 'shift'
   _1 'done'
-  _1 'unset OPTARG'
-  _1 '[ $# -eq 0 ] && return 0'
+  _1 '[ $# -eq 0 ] && { unset OPTARG; return 0 ;}'
   [ "$_error" ] && _1 "$_error" '"$@" >&2 && exit 1'
   _1 'case $2 in'
   _2 "unknown) echo \"unrecognized option '\$1'\" ;;"
