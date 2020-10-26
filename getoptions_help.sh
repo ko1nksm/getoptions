9e492e67395ac470d862d57eb4b55422c2e81fd1
Koichi Nakashima <koichi@nksm.name>
2020-10-27 00:59:31 +0900

    setup: Add leading attribute

--- getoptions_help.sh
+++ getoptions_help.sh
@@ -2,7 +2,7 @@
 # URL: https://github.com/ko1nksm/getoptions (v1.1.0)
 # License: Creative Commons Zero v1.0 Universal
 getoptions_help() {
-	width=30 plus='' here='GETOPTIONS-HEREDOC'
+	width=30 plus='' here='GETOPTIONS-HEREDOC' leading='  '
 
 	pad() { p=$2; while [ ${#p} -lt "$3" ]; do p="$p "; done; eval "$1=\$p"; }
 
@@ -11,20 +11,20 @@ getoptions_help() {
 		while [ $# -gt 0 ] && i=$1 && shift && [ ! "$i" = '--' ]; do
 			case $i in
 				--*) pad sw "$sw${sw:+, }" $((${plus:+4}+4)); sw="$sw$i" ;;
-				-? ) sw="$sw${sw:+, }$i" ;;
-				+? ) [ ! "$plus" ] || { pad sw "$sw${sw:+, }" 4; sw="$sw$i"; } ;;
-				*  ) eval "${i%%:*}=\${i#*:}" ;;
+				-?) sw="$sw${sw:+, }$i" ;;
+				+?) [ ! "$plus" ] || { pad sw "$sw${sw:+, }" 4; sw="$sw$i"; } ;;
+				*) eval "${i%%:*}=\${i#*:}"
 			esac
 		done
 		[ "$hidden" ] && return 0
 
 		[ "$label" ] || case $_type in
 			setup | msg) label='' _width=0 ;;
-			flag | disp) label="$sw" ;;
-			param      ) label="$sw $var" ;;
-			option     ) label="$sw""[=$var]" ;;
+			flag | disp) label="$sw " ;;
+			param) label="$sw $var " ;;
+			option) label="${sw}[=$var] "
 		esac
-		pad label "${label:+  }$label" "$_width"
+		pad label "${label:+$leading}$label" "$_width"
 		[ ${#label} -le "$_width" ] && [ $# -gt 0 ] && label="$label$1" && shift
 		echo "$label"
 		pad label '' "$_width"
