43cc48823641bfa0feecbc14a38a0c2001d88cb4
Koichi Nakashima <koichi@nksm.name>
2020-11-01 13:01:15 +0900

    Update samples

--- sample/generator.sh
+++ sample/generator.sh
@@ -7,17 +7,19 @@ VERSION=0.1
 
 . ./lib/getoptions.sh
 . ./lib/getoptions_help.sh
+. ./lib/getoptions_abbr.sh
 
 parser_definition() {
-  setup   REST plus:true help:usage -- "Usage: ${2##*/} [options...] [arguments...]"
-  msg -- '' 'getoptions sample' ''
-  msg -- 'Options:'
-  flag    FLAG    -f +f --{no-}flag                         -- "expands to --flag and --no-flag"
-  flag    VERBOSE -v    --verbose   counter:true init:=0    -- "e.g. -vvv is verbose level 3"
-  param   PARAM   -p    --param     pattern:"foo | bar"     -- "accepts --param value / --param=value"
-  option  OPTION  -o    --option    on:"default"            -- "accepts -ovalue / --option=value"
-  disp    :usage  -h    --help
-  disp    VERSION       --version
+	setup   REST plus:true help:usage abbr:true error alt:true \
+		-- "Usage: ${2##*/} [options...] [arguments...]"
+	msg -- '' 'getoptions sample' ''
+	msg -- 'Options:'
+	flag    FLAG    -f +f --{no-}flag                         -- "expands to --flag and --no-flag"
+	flag    VERBOSE -v    --verbose   counter:true init:=0    -- "e.g. -vvv is verbose level 3"
+	param   PARAM   -p    --param     pattern:"foo | bar"     -- "accepts --param value / --param=value"
+	option  OPTION  -o    --option    on:"default"            -- "accepts -ovalue / --option=value"
+	disp    :usage  -h    --help
+	disp    VERSION       --version
 }
 
 number() { case $OPTARG in (*[!0-9]*) return 1; esac; }
