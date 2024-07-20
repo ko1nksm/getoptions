fe4a80ff0676df53620e87d64e4cc04ba55b0537
Koichi Nakashima <koichi@nksm.name>
2024-07-20 17:42:42 +0900

    Revert "Revert "Improve performance""
    This reverts commit d3289095f47b35721c7833d9c53a306cb5975bab.

--- lib/getoptions_base.sh
+++ lib/getoptions_base.sh
@@ -6,8 +6,8 @@ getoptions() {
 	[ $# -lt 2 ] && set -- "${1:?No parser definition}" -
 	[ "$2" = - ] && _def=getoptions_parse
 
-	_0() { echo "$@"; }
-	for i in 1 2 3 4 5; do eval "_$i() { _$((${i-}-1)) \"	\$@\"; }"; done
+	i='					'
+	while eval "_${#i}() { echo \"$i\$@\"; }"; [ "$i" ]; do i=${i#?}; done
 
 	quote() {
 		q="$2'" r=''
